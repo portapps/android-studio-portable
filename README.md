85939c4a452de2bdff71132672c24ca57d204e94
CrazyMax <crazy-max@users.noreply.github.com>
2020-12-02 09:20:29 +0100

    Portapps 3.0.0

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 <p align="center">
   <a href="https://portapps.io/app/android-studio-portable/#download"><img src="https://img.shields.io/github/release/portapps/android-studio-portable.svg?style=flat-square" alt="GitHub release"></a>
   <a href="https://portapps.io/app/android-studio-portable/#download"><img src="https://img.shields.io/github/downloads/portapps/android-studio-portable/total.svg?style=flat-square" alt="Total downloads"></a>
-  <a href="https://travis-ci.com/portapps/android-studio-portable"><img src="https://img.shields.io/travis/com/portapps/android-studio-portable/master.svg?style=flat-square" alt="Build Status"></a>
+  <a href="https://github.com/portapps/android-studio-portable/actions?workflow=build"><img src="https://img.shields.io/github/workflow/status/portapps/android-studio-portable/build?label=build&logo=github&style=flat-square" alt="Build Status"></a>
   <a href="https://goreportcard.com/report/github.com/portapps/android-studio-portable"><img src="https://goreportcard.com/badge/github.com/portapps/android-studio-portable?style=flat-square" alt="Go Report"></a>
   <a href="https://app.codacy.com/gh/portapps/android-studio-portable"><img src="https://img.shields.io/codacy/grade/8f583d9581754ffca31c72f396d8da23.svg?style=flat-square" alt="Code Quality"></a>
   <br /><a href="https://github.com/sponsors/crazy-max"><img src="https://img.shields.io/badge/sponsor-crazy--max-181717.svg?logo=github&style=flat-square" alt="Become a sponsor"></a>
--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 	"os"
 	"strings"
 
-	"github.com/portapps/portapps/v2"
-	"github.com/portapps/portapps/v2/pkg/log"
-	"github.com/portapps/portapps/v2/pkg/utl"
+	"github.com/portapps/portapps/v3"
+	"github.com/portapps/portapps/v3/pkg/log"
+	"github.com/portapps/portapps/v3/pkg/utl"
 )
 
 var (
@@ -46,14 +46,14 @@ idea.log.path={{ DATA_PATH }}/log`, "{{ DATA_PATH }}", utl.FormatUnixPath(app.Da
 	}
 
 	// https://developer.android.com/studio/command-line/variables
-	utl.OverrideEnv("ANDROID_HOME", utl.PathJoin(app.DataPath, "sdk"))
-	utl.OverrideEnv("ANDROID_SDK_ROOT", utl.PathJoin(app.DataPath, "sdk"))
-	utl.OverrideEnv("ANDROID_SDK_HOME", utl.PathJoin(app.DataPath, ".android"))
-	utl.OverrideEnv("GRADLE_USER_HOME", utl.PathJoin(app.DataPath, ".gradle"))
+	os.Setenv("ANDROID_HOME", utl.PathJoin(app.DataPath, "sdk"))
+	os.Setenv("ANDROID_SDK_ROOT", utl.PathJoin(app.DataPath, "sdk"))
+	os.Setenv("ANDROID_SDK_HOME", utl.PathJoin(app.DataPath, ".android"))
+	os.Setenv("GRADLE_USER_HOME", utl.PathJoin(app.DataPath, ".gradle"))
 
 	// https://developer.android.com/studio/intro/studio-config
-	utl.OverrideEnv("STUDIO_PROPERTIES", studioPropPath)
-	utl.OverrideEnv("STUDIO_VM_OPTIONS", utl.PathJoin(app.DataPath, vmOptionsFile))
+	os.Setenv("STUDIO_PROPERTIES", studioPropPath)
+	os.Setenv("STUDIO_VM_OPTIONS", utl.PathJoin(app.DataPath, vmOptionsFile))
 	if !utl.Exists(utl.PathJoin(app.DataPath, vmOptionsFile)) {
 		utl.CopyFile(utl.PathJoin(app.AppPath, "bin", "studio64.exe.vmoptions"), utl.PathJoin(app.DataPath, vmOptionsFile))
 	} else {
